ba7c0c37743d3d974b4372d3da3d64baffc247a0
bcssp10 <znmansoor@gmail.com>
2017-09-23 20:25:25 +0500

    no message

--- src/main/java/com/codentech/mars2/Mars2Application.java
+++ src/main/java/com/codentech/mars2/Mars2Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.annotation.ComponentScan;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 
